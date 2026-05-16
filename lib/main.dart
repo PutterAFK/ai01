0e6432b24cb24dbdc44abac99636dd14d144083f
PutterAFK <putter.6712@gmail.com>
2026-05-16 19:51:45 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:mindmate/providers/chat_provider.dart';
+import 'package:mindmate/providers/theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
